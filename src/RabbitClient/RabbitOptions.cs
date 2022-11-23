1927700bb3e883b30300295f747cab05df7c8723
muhammad <mj.gholami@snapp.express>
2022-11-23 11:21:52 +0330

    Add Default Values to RabbitOptions.cs

--- src/RabbitClient/RabbitOptions.cs
+++ src/RabbitClient/RabbitOptions.cs
@@ -6,10 +6,10 @@ public class RabbitOptions
     public string HostName { get; set; }
     public string UserName { get; set; }
     public string Password { get; set; }
-    public string Vhost { get; set; }
+    public string Vhost { get; set; } = "/";
     public bool AutoDelete { get; set; }
-    public bool Durable { get; set; }
+    public bool Durable { get; set; } = true;
     public bool Exclusive { get; set; }
-    public int RetryCount { get; set; }
+    public int RetryCount { get; set; } = 3;
     public bool UseSecondaryConnectionForConsumers { get; set; }
 }
